b4fa3e46ccb6e6d37c0129708d0c727973a1fde6
Gameplushy <victor_f@orange.fr>
2023-10-13 12:39:54 +0200

    Create CRUD for Actors + Actor class for requests

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Filmotheque.Models;
+using Filmotheque.Data;
+using Filmotheque.Models.Requests;
+
+namespace Filmotheque.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorController : ControllerBase
+    {
+        private readonly APIContext _context;
+
+        public ActorController(APIContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpPost]
+        public JsonResult Create(ActorRequest actor)
+        {
+            var res = _context.Actors.Add(actor.ToActor());
+            _context.SaveChanges();
+            return new JsonResult(Ok(res.Entity));
+        }
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            return new JsonResult(Ok(_context.Actors));
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public JsonResult Get(int id)
+        {
+            Actor? a = _context.Actors.Find(id);
+            if (a == null)
+                return new JsonResult(NotFound($"Actor of id {id} not found."));
+            return new JsonResult(Ok(a));
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        public JsonResult Patch(int id,ActorRequest actor)
+        {
+            Actor? oldActor = _context.Actors.Find(id);
+            if (oldActor == null)
+                return new JsonResult(NotFound($"Actor of id {id} not found."));
+            if(actor.FirstName != oldActor.FirstName)
+                oldActor.FirstName = actor.FirstName;
+            if(actor.LastName != oldActor.LastName)
+                oldActor.LastName = actor.LastName;
+            if(actor.BirthDate != oldActor.BirthDate)
+                oldActor.BirthDate = actor.BirthDate;
+            _context.SaveChanges();
+            return new JsonResult(Ok(actor));
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public JsonResult Delete(int id)
+        {
+            Actor? oldActor = _context.Actors.Find(id);
+            if (oldActor == null) 
+                return new JsonResult(NotFound($"Actor of id {id} not found."));
+            _context.Actors.Remove(oldActor);
+            _context.SaveChanges();
+            return new JsonResult(Ok());
+        }
+    }
+}
---
+++ Models/Requests/ActorRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Filmotheque.Models.Requests
+{
+    public class ActorRequest
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Actor ToActor()
+        {
+            return new Actor()
+            {
+                Id = 0,
+                FirstName = FirstName,
+                LastName = LastName,
+                BirthDate = BirthDate
+            };
+        }
+    }
+}
