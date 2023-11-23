157e95115c4c44feec96c2be22d6689989d74612
Gameplushy <victor_f@orange.fr>
2023-11-23 11:22:17 +0100

    Use InMemory instead of SQLServer

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using Filmotheque.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<APIContext>(opt => opt.UseSqlServer("Server=(local)\\SQLEXPRESS;Database=FILMOTHEQUE;Integrated Security=SSPI;TrustServerCertificate=True"));
+builder.Services.AddDbContext<APIContext>(opt => opt.UseInMemoryDatabase("Filmotheque"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
